d2f2cc9a8912ee46bc9cfcc41ce0b379ebcb4384
Rselless <selles.rafael27@gmail.com>
2025-08-07 09:35:31 -0300

    fix: :art: Bug Fix Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,19 @@
 function Home() {
     return (
         <>
-            <div style={{
-                backgroundColor: "#f3cbf7",
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center",
-                margin: "0",
-                padding: "2rem",
-            }}>
+        <div className="w-screen h-20  bg-purple-600 flex items-center justify-center">
+            </div>
+            <div className="w-screen  bg-purple-300 flex justify-center">
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         fontSize: "2.5rem",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <h2>Se aconchegue!</h2>
-                        <p>Expresse aqui seus pensamentos e opiniões</p>
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui seus pensamentos e opniões</p>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
+                    <div className="max-w-7xl  flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
                             width="400px"
                         />
                     </div>
